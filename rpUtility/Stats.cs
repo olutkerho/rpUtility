acb8d6fe98ce744a80444548df599daf704bb791
Leo Qvarnström <leo.w.qvarnstrom@gmail.com>
2019-01-16 17:40:06 +0200

    +spells.cs

--- rpUtility/Stats.cs
+++ rpUtility/Stats.cs
@@ -4,35 +4,39 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-public class Stats{
-    private int Strength;
-    private int Dexterity;
-    private int Constitution;
-    private int Intelligence;
-    private int Wisdom;
-    private int Charisma;
-    private int ProfiencyBonus;
-    private int ArmorClass;
-    private int Speed;
-    private int HpMax;
-    private int HpCurrent;
-    private int HpTemp;
-    private List<string> Traits;
-    private List<string> Proficiencies;
-    //Skills
-    //Attacks
-    //Spells
-    //SpellSlots
+namespace rpUtility {
+    public class Stats {
+        private int Strength;
+        private int Dexterity;
+        private int Constitution;
+        private int Intelligence;
+        private int Wisdom;
+        private int Charisma;
+        private int ProfiencyBonus;
+        private int ArmorClass;
+        private int Speed;
+        private int HpMax;
+        private int HpCurrent;
+        private int HpTemp;
+        private List<string> Traits;
+        private List<string> Proficiencies;
+        private List<Spells> Spells;
+        //Skills
+        //Attacks
+        //Spells
+        //SpellSlots
 
-    private int StrengthMod;
-    private int DexterityMod;
-    private int ConstitutionMod;
-    private int IntelligenceMod;
-    private int IntelligenceMod;
-    private int WisdomMod;
-    private int PassivePerception;
+        private int StrengthMod;
+        private int DexterityMod;
+        private int ConstitutionMod;
+        private int IntelligenceMod;
+        private int IntelligenceMod;
+        private int WisdomMod;
+        private int PassivePerception;
 
-	public Stats(){
+        public Stats() {
 
-	}
+        }
+    }
+
 }
