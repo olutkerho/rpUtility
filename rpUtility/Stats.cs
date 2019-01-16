6aa17a6fdda885c16065cb9f5021dd5061b57197
slamchan <43639359+slamchan@users.noreply.github.com>
2019-01-16 15:18:38 +0200

    lisätty stats.cs

---
+++ rpUtility/Stats.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Stats{
+    private int Strength;
+    private int Dexterity;
+    private int Constitution;
+    private int Intelligence;
+    private int Wisdom;
+    private int Charisma;
+    private int ProfiencyBonus;
+    private int ArmorClass;
+    private int Speed;
+    private int HpMax;
+    private int HpCurrent;
+    private int HpTemp;
+    private List<string> Traits;
+    private List<string> Proficiencies;
+    //Skills
+    //Attacks
+    //Spells
+    //SpellSlots
+
+    private int StrengthMod;
+    private int DexterityMod;
+    private int ConstitutionMod;
+    private int IntelligenceMod;
+    private int IntelligenceMod;
+    private int WisdomMod;
+    private int PassivePerception;
+
+	public Stats(){
+
+	}
+}
