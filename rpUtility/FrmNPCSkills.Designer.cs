7b36c183f77055e59cb78c9c4166af6239f7d8c8
Leo Qvarnström <leo.w.qvarnstrom@gmail.com>
2019-01-18 10:43:35 +0200

    remover .suo, storage.ide-shm, storage.ide-wal, VSWorkspaceState.json

--- rpUtility/FrmNPCSkills.Designer.cs
+++ rpUtility/FrmNPCSkills.Designer.cs
@@ -24,6 +24,7 @@
         /// </summary>
         private void InitializeComponent() {
             this.flpSkills = new System.Windows.Forms.FlowLayoutPanel();
+            this.btnSetSkills = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // flpSkills
@@ -33,11 +34,22 @@
             this.flpSkills.Size = new System.Drawing.Size(200, 400);
             this.flpSkills.TabIndex = 0;
             // 
+            // btnSetSkills
+            // 
+            this.btnSetSkills.Location = new System.Drawing.Point(12, 422);
+            this.btnSetSkills.Name = "btnSetSkills";
+            this.btnSetSkills.Size = new System.Drawing.Size(199, 55);
+            this.btnSetSkills.TabIndex = 1;
+            this.btnSetSkills.Text = "set skills";
+            this.btnSetSkills.UseVisualStyleBackColor = true;
+            this.btnSetSkills.Click += new System.EventHandler(this.btnSetSkills_Click);
+            // 
             // FrmNPCSkills
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(321, 504);
+            this.Controls.Add(this.btnSetSkills);
             this.Controls.Add(this.flpSkills);
             this.Name = "FrmNPCSkills";
             this.Text = "FrmNPCSkills";
@@ -48,5 +60,6 @@
         #endregion
 
         private System.Windows.Forms.FlowLayoutPanel flpSkills;
+        private System.Windows.Forms.Button btnSetSkills;
     }
 }
